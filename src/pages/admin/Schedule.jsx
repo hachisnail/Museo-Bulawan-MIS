9b89a48e857c8a684752b7ecaeb4a8eba60c3b80
rhettrina <michael18ricafrente@gmail.com>
2025-04-24 00:03:26 +0800

    layout changes schedule

--- src/pages/admin/Schedule.jsx
+++ src/pages/admin/Schedule.jsx
@@ -12,8 +12,8 @@ const Schedule = () => {
 
         <div className='w-full bg-[#F0F0F0] min-h-full h-full flex flex-col gap-y-10 px-7 pb-7 pt-[4rem] overflow-scroll'>
           <span className=' text-5xl font-semibold text-black'>Schedule</span>
-          <div className='w-full h-full flex flex-col xl:flex-row  gap-y-5 xl:gap-y-0 justify-between sm:px-12'> 
-            
+          <div className='w-full h-full flex flex-col xl:flex-row  gap-y-5 xl:gap-y-0 justify-between sm:px-12'>
+
             <div className='sm:[w-31rem] h-full flex flex-col justify-between items-center pt-20'>
               {/* left tab */}
               <div className='min-w-[31rem] max-w-[31rem] min-h-[28rem] flex flex-col justify-between gap-y-5'>
@@ -34,11 +34,11 @@ const Schedule = () => {
 
                 </div>
 
-                
+
               </div>
             </div>
 
-            <div className='min-w-full xl:min-w-[55rem] max-w-[55rem] h-full flex flex-col gap-y-8'>
+            <div className='min-w-full xl:min-w-[55rem] xl:max-w-[55rem] 2xl:max-w-[90rem] 2xl:min-w-[90rem] h-full flex flex-col gap-y-8'>
               {/* middle tab */}
               <div className='w-full min-h-[5rem] flex flex-col justify-between'>
                 <span className='text-4xl font-bold text-black'>Today’s Schedule</span>
@@ -62,15 +62,15 @@ const Schedule = () => {
                 </div>
 
                 <div className='w-full h-full flex flex-col gap-y-2'>
-                  <div className='w-full min-h-[4rem] rounded-xl bg-black flex items-center px-5 shadow-xl'> 
+                  <div className='w-full min-h-[4rem] rounded-xl bg-black flex items-center px-5 shadow-xl'>
                     <span className='text-white text-3xl font-semibold'>Good Morning!</span>
                   </div>
-                  <div className='w-full min-h-[4rem] rounded-xl bg-black flex items-center px-5 shadow-xl'> 
+                  <div className='w-full min-h-[4rem] rounded-xl bg-black flex items-center px-5 shadow-xl'>
                     <span className='text-white text-3xl font-semibold'>Hello</span>
                   </div>
-                  
-                  <div className=' h-[41rem] min-w-full shadow-xl bg-white rounded-xl'>
-                  {/* devil display cards */}
+
+                  <div className='2xl:h-[53rem] h-[41rem] min-w-full shadow-xl bg-white rounded-xl'>
+                    {/* devil display cards */}
 
 
 
@@ -78,7 +78,7 @@ const Schedule = () => {
                   </div>
 
 
-                  <div className='w-full min-h-[4rem] rounded-xl bg-black flex items-center px-5 shadow-xl'> 
+                  <div className='w-full min-h-[4rem] rounded-xl bg-black flex items-center px-5 shadow-xl'>
                     <span className='text-white text-3xl font-semibold'>Hello</span>
                   </div>
                 </div>
@@ -99,16 +99,16 @@ const Schedule = () => {
                 <div className='w-full h-fit'>
                   <span className='text-2xl font-semibold'>Add a Schedule for <span className='text-[#A6A3F6]'>April 17</span></span>
                 </div>
-                  {/* form for event title decription and time start, end */}
+                {/* form for event title decription and time start, end */}
               </div>
-              
+
               <div className='w-full h-full shadow-xl bg-white rounded-xl'>
                 {/* School Fieldtrip */}
               </div>
             </div>
           </div>
 
-            
+
 
         </div>
 
